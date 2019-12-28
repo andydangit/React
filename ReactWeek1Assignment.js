1735bde30a6da2ea425e8a1154aabcd79d4a94ef
Pandoragear <56464282+Pandoragear@users.noreply.github.com>
2019-12-28 12:17:32 -0600

    Add files via upload

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,29 @@
+
+class Student {
+    constructor(name, email, community){
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+class bootcamp {
+    constructor(name, level, students = []){
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+    registerStudent(student) {
+        if (this.students.filter(s => s.email === student.email)){
+            console.log('this student ${student.email} is registered already');
+        } else {
+            this.students.push(student);
+            console.log('Registering ${student.email} to the bootcamp ${this.name}.');
+        }
+        return this.students;
+    }
+}
+
+// note 
+
+
