c5bef8306e9d6a0e9eb955ef38d73f4b497ec69d
Andy Dang <andy.dang.it@gmail.com>
2019-12-28 11:39:24 -0600

    testing

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -17,10 +17,3 @@ class bootcamp {
 
 const registerStudent = bootcamp.filter(name=> name[0] === 'K');
 
-
-
-// const webDevFudamentals = new bootcamp("Web Development Fundamentals", "beginner");
-
-// const fullStack = new Bootcamp("Full Stack Development", "Beginner");
-
-// const Andy = new student("Andy", "andy@email.com", "Texas");
