2833e410efe6af508d26b9776cf4f6ba2e1ba007
Andy Dang <andy.dang.it@gmail.com>
2019-12-28 12:04:35 -0600

    12:04 pm

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -8,12 +8,23 @@ class Student {
 }
 
 class bootcamp {
-    constructor(name, level, students){
+    constructor(name, level, students = []){
         this.name = name;
         this.level = level;
         this.students = students;
     }
+    registerStudent(student) {
+        if (this.students.filter(s => s.email === student.email)){
+            console.log('the student ${student.email} is already registered!');
+        } else {
+            this.students.push(student);
+            console.log('Registering ${student.email} to the bootcamp ${this.name}.');
+        }
+        return this.students;
+    }
 }
 
-const registerStudent = bootcamp.filter(name=> name[0] === 'K');
+
+
+// const registerStudent = bootcamp.filter(name=> name[0] === 'K');
 
