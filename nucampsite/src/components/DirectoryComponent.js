0822cce0b5600976761a441194825628b13d1737
Andy Dang <andy.dang.it@gmail.com>
2020-01-04 13:16:39 -0600

    Week 2

--- nucampsite/src/components/DirectoryComponent.js
+++ nucampsite/src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent'
 
 class Directory extends Component {
     constructor(props) {
@@ -8,53 +9,35 @@ class Directory extends Component {
             selectedCampsite: null
         };
     }
+
+
     onCampsiteSelect(campsite) {
         this.setState({selectedCampsite: campsite});
     }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
-    }
-
+  
     render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                    <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                    <CardImgOverlay>
-                    <CardTitle> {campsite.name}</CardTitle>
-                    <CardText>{campsite.description} </CardText>
-                    </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
+      const directory = this.props.campsites.map(campsite => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
-            </div>
+          <div key={campsite.id} className="col-md-5 m-1">
+            <Card onClick={() => this.onCampsiteSelect(campsite)}>
+              <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+              <CardImgOverlay>
+              <CardTitle>{campsite.name}</CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </div>
         );
+      });
+  
+      return (
+        <div className="container">
+          <div className="row">
+            {directory}
+          </div>
+          <CampsiteInfo campsite={this.state.selectedCampsite} />
+        </div>
+      );
     }
-}
-
-export default Directory;
+  }
+  
+  export default Directory;
