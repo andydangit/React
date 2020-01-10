c91cc62378a6f2daf19b363f8fc848ab0bcd2a86
Andy Dang <andy.dang.it@gmail.com>
2020-01-09 21:43:03 -0600

    Functional Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
-    renderCampsite(campsite){
+
+    function RenderCampsite({campsite}){
         return(
         <div className= "col-md-5 m-1">
             <Card>
@@ -18,7 +18,7 @@ class CampsiteInfo extends Component {
     }
 
     //call comment from campsites.js
-    renderComments(comments){
+    function RenderComments({comments}){
         if(comments){
             return(
                 <div className="col-md-5 m-1">
@@ -36,18 +36,18 @@ class CampsiteInfo extends Component {
     }
 
     //show information on site
-    render () {
-        if(this.props.campsite){
+    function CampsiteInfo(props) {
+        if(props.campsite){
            return(
                <div className="container">
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div> 
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
+                    </div> 
                 </div> 
            );
         }
         return (<div/>);
     }
-}
+
 export default CampsiteInfo;
