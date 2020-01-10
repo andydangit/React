cb7382235f2be564e41d1f950f63a36b6ac67c4c
Andy Dang <andy.dang.it@gmail.com>
2020-01-09 22:17:10 -0600

    Header and Footer

--- nucampsite/src/index.js
+++ nucampsite/src/index.js
@@ -6,6 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
