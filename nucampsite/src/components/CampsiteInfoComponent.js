72af8fbfac887a7e4cff29414ad6f0dac873e52d
Andy Dang <andy.dang.it@gmail.com>
2020-01-09 20:31:27 -0600

    Container Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -38,10 +38,14 @@ class CampsiteInfo extends Component {
     //show information on site
     render () {
         if(this.props.campsite){
-           return <div className="row">
-               {this.renderCampsite(this.props.campsite)}
-               {this.renderComments(this.props.campsite.comments)}
-           </div>  
+           return(
+               <div className="container">
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div> 
+                </div> 
+           );
         }
         return (<div/>);
     }
