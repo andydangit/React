c65377daee0f42c1dd063cb43a7c0445093615fd
Andy Dang <andy.dang.it@gmail.com>
2020-01-18 13:07:22 -0600

    Combining Reducers

---
+++ nucampsite/src/redux/comments.js
@@ -0,0 +1,8 @@
+import { COMMENTS } from '../shared/comments';
+
+export const comments = (state = COMMENTS, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+};
--- nucampsite/src/redux/configureStore.js
+++ nucampsite/src/redux/configureStore.js
@@ -1,10 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, CombineReducers } from 'redux';
+import { Campsites } from './campsites';
+import { Comment } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        CombineReducers({
+            campsites: Campsites,
+            comments: Comment,
+            partners: Partners,
+            Promotions: Promotions
+        })
     );
 
     return store;
