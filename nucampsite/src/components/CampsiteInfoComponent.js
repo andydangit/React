450a1af8b4d46248b7f6a7144815af8aaf15e773
Andy Dang <andy.dang.it@gmail.com>
2020-01-20 18:41:06 -0600

    Redux Thunk

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,
         Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 const required = val => val && val.length;
@@ -138,6 +139,26 @@ const minLength = len => val => val && (val.length >= len);
 
     //show information on site
     function CampsiteInfo(props) {
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h4>{props.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
         if(props.campsite){
            return(
                <div className="container">
--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchCampsites } from '../redux/ActionCreators';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
@@ -24,19 +24,26 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
-    addComment: (campsiteID, rating, author, text) => (addComment(campsiteID, rating, author, text))
+    addComment: (campsiteID, rating, author, text) => (addComment(campsiteID, rating, author, text)),
+    fetchCampsites: () => (fetchCampsites())
 };
 
 class Main extends Component {
 
+    componentDidMount() {
+        this.props.fetchCampsites();
+    }
+    
     render() {
 
         const HomePage = () => {
             return (
                 <Home 
-                    campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
-                    promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
-                    partner={this.props.partners.filter(partner => partner.featured)[0]}
+                campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                campsitesLoading={this.props.campsites.isLoading}
+                campsitesErrMess={this.props.campsites.errMess}
+                promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.props.partners.filter(partner => partner.featured)[0]}
                 />
             );
         }
@@ -45,9 +52,11 @@ class Main extends Component {
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
-                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-                    addComment={this.props.addComment}
+                campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                isLoading={this.props.campsites.isLoading}
+                errMess={this.props.campsites.errMess}
+                comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                addComment={this.props.addComment}
                 />
             );
         };    
