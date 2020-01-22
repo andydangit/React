cc580f955dc23b48ac8ab069b0f35b141c54c094
Andy Dang <andy.dang.it@gmail.com>
2020-01-22 00:32:38 -0600

    React Animation Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const required = val => val && val.length;
@@ -17,13 +18,19 @@ const minLength = len => val => val && (val.length >= len);
     function RenderCampsite({campsite}){
         return(
         <div className= "col-md-5 m-1">
-            <Card>
-                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>
-        </div>            
+          <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                    <Card>
+                        <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardText>{campsite.description}</CardText>
+                        </CardBody>
+                    </Card>
+            </FadeTransform>
+        </div>           
         )
     }
 
@@ -33,19 +40,25 @@ const minLength = len => val => val && (val.length >= len);
             return(
                 <div className="col-md-5 m-1">
                     <h4> Comments </h4>
-                    {comments.map(comment => 
-                        <div key={comment.id}> 
-                        {comment.text} <br /> 
-                        -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                        <hr />
-                        </div>)}
-                        <CommentForm  campsiteID={campsiteID} postComment={postComment} />
+                    <Stagger in>
+                    {comments.map(comment => {
+                        return (
+                            <Fade in key={comment.id}>
+                                <div>
+                                    <p>{comment.text}<br />
+                                        -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    </p>
+                                </div>
+                            </Fade>
+                            );
+                        })}
+                    </Stagger>
+                    <CommentForm campsiteID={campsiteID} postComment={postComment} />
                 </div>
             );
-        };
-            return(<div/>);
-
-    }
+        }
+    return(<div/>);
+}
 
 
     //Adding Comment button and show comment
