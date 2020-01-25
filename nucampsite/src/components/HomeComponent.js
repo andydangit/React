1eaaae10e6931e0eadd15ea7c8d7f32466bf4a0a
Andy Dang <andy.dang.it@gmail.com>
2020-01-25 13:53:35 -0600

    Week 5 Assignment

--- nucampsite/src/components/HomeComponent.js
+++ nucampsite/src/components/HomeComponent.js
@@ -20,7 +20,7 @@ function RenderCard({item, isLoading, errMess}) {
         <FadeTransform
         in
         transformProps={{
-            exitTransform: 'scale(0.5) translateY(50%)'
+            exitTransform: 'scale(2) translateY(90%)'
         }}>
             <Card>
                 <CardImg src={baseUrl + item.image} alt={item.name} />
@@ -52,7 +52,10 @@ function Home(props) {
                         />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
+                    <RenderCard item={props.partner} 
+                    isLoading={props.partnersLoading}
+                    errMess={props.partnersErrMess}
+                    />
                 </div>
             </div>
         </div>
